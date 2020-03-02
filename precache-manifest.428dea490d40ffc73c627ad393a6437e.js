4b04615b194ab3a01f060df016cf9ff87e70e42b
JaegerFlorian <florian.jaeger@pcode.at>
2020-03-02 13:43:00 +0100

    Updates

---
+++ precache-manifest.428dea490d40ffc73c627ad393a6437e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d524a355796f8335b099d1b85ef418",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7e8b480f20496850989",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "511faaaf5138074d07cc",
+    "url": "/static/js/2.1c6feeb7.chunk.js"
+  },
+  {
+    "revision": "9aea971713c6773bef69d3e2a3c91fd5",
+    "url": "/static/js/2.1c6feeb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7e8b480f20496850989",
+    "url": "/static/js/main.3864c7d5.chunk.js"
+  },
+  {
+    "revision": "2947b7cce41296178595",
+    "url": "/static/js/runtime-main.c351ddc3.js"
+  },
+  {
+    "revision": "2b660318771835acfc1c81fedac52380",
+    "url": "/static/media/lamp.2b660318.png"
+  }
+]);
