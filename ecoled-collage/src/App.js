f095ea2a2296c38317293a2ce1921fa68a4eb8bc
JaegerFlorian <florian.jaeger@pcode.at>
2020-04-15 17:01:15 +0200

    Update App.js

--- ecoled-collage/src/App.js
+++ ecoled-collage/src/App.js
@@ -173,6 +173,12 @@ const Lamp = ({
       />
       {isSelected && (
         <Transformer
+          enabledAnchors={[
+            'top-left',
+            'top-right',
+            'bottom-left',
+            'bottom-right',
+          ]}
           ref={trRef}
           boundBoxFunc={(oldBox, newBox) => {
             if (newBox.width < 5 || newBox.height < 5) {
